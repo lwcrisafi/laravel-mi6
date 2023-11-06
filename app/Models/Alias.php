34b23524efc4dcfcefcfc8b59caf6e2359810679
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-06 13:09:50 +0100

    added relationship

--- app/Models/Alias.php
+++ app/Models/Alias.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alias extends Model
 {
-    use HasFactory;
+    
+    public function person() 
+    {
+        return $this->belongsTo(Person::class);
+    }
 }
