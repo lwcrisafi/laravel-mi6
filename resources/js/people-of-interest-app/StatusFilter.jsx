a419a37c022bd8ce3fd7e5ba45fef24e320835cb
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-07 09:59:40 +0100

    updated status filter

--- resources/js/people-of-interest-app/StatusFilter.jsx
+++ resources/js/people-of-interest-app/StatusFilter.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function StatusFilter() {
+    const [statuses,setStatuses]= useState([]);
+     const loadStatuses = async () => {
+     const response = await fetch('/api/statuses');
+     const data = await response.json();
+     setStatuses(data);
+
+     }
+     useEffect(() => {
+        loadStatuses();
+     }, []);
     return (
         <>
-            <div className="status-filter">This is the status filter</div>
+            <div className="status-filter">
+                {statuses.map((status)=>(
+                    <div key={status.id}
+                    className={ 'status-filter__status' }
+                    >
+                        { status.name }
+                    </div>
+                )
+                )
+                }
+                </div>
         </>
     );
 }
