7834ec6ee7659800412b3bbc0b5237f47eed9088
Ephram Daniels <linguistnick7@gmail.com>
2023-11-07 11:27:18 +0100

    fixed fetch issues

--- resources/js/people-of-interest-app/StatusFilter.jsx
+++ resources/js/people-of-interest-app/StatusFilter.jsx
@@ -1,30 +1,38 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
-export default function StatusFilter() {
-    const [statuses,setStatuses]= useState([]);
-     const loadStatuses = async () => {
-     const response = await fetch('/api/statuses');
-     const data = await response.json();
-     setStatuses(data);
-
-     }
-     useEffect(() => {
+export default function StatusFilter({ selectedStatus, setSelectedStatus}) {
+    const [statuses, setStatuses] = useState([]);
+    const loadStatuses = async () => {
+        const response = await fetch(
+            "/api/statuses"
+        );
+        const data = await response.json();
+        setStatuses(data);
+    };
+    
+    useEffect(() => {
         loadStatuses();
-     }, []);
+    }, []);
+
+    const handleStatusClick = (id) => {
+        setSelectedStatus(id);
+    };
+
     return (
         <>
             <div className="status-filter">
-                {statuses.map((status)=>(
-                    <div key={status.id}
-                    className={ 'status-filter__status' }
+                {statuses.map((status) => (
+                    <div
+                        key={status.id}
+                        className={"status-filter__status" + (status.id == selectedStatus ? " status-filter__status_selected" : '')}
+                        onClick={ () => handleStatusClick(status.id)}
                     >
-                        { status.name }
+                        {status.name}
                     </div>
-                )
-                )
-                }
-                </div>
+                ))}
+            </div>
         </>
     );
 }
